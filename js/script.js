b5468010e93d9470d46190b98c2b1d4aa336072e
Ryley anne Bridges <932400076@bun2-206-12.local>
2019-06-06 11:39:43 +1200

    Updated

--- js/script.js
+++ js/script.js
@@ -3,15 +3,18 @@ var array = [];
 
 function addItem(){
   array[x] = document.getElementById('itemInput').value;
+  var optionSelected = document.getElementById('category').value;
   // array.push(array[x]);
   document.getElementById('itemInput').value = '';
 
   var addToShopping = document.getElementById('shoppingList');
   addToShopping.innerHTML += '<ul>';
-  addToShopping.innerHTML += '<li>' + array[x] + '</li> <button class="btn btn-outline-danger btn-sm"><i class="fas fa-minus" onclick="removeItem();"></i></button>';
+  addToShopping.innerHTML += '<li>';
+  addToShopping.innerHTML += array[x];
+    addToShopping.innerHTML += '<li class="sub-list"> Category: ' + optionSelected + '</li>';
+  addToShopping.innerHTML += '</li>';
+  addToShopping.innerHTML += '<button class="btn btn-outline-danger btn-sm"><i class="fas fa-minus" onclick="removeItem();"></i></button>';
 
-  var optionSelected = document.getElementById('category');
-  console.log(optionSelected);
 };
 
 function removeItem(){
