ab132b4a9426591829b5f1a8ced867d2c3e17631
Ryley anne Bridges <932400076@bun2-206-12.local>
2019-06-07 11:01:06 +1200

    Updated

--- js/script.js
+++ js/script.js
@@ -17,13 +17,13 @@ var list = [
 function showList(){
   for (var i = 0; i < list.length; i++) {
     var addToShopping = document.getElementById('shoppingList');
-    addToShopping.innerHTML = '<ul>';
-      addToShopping.innerHTML = '<li>';
-      addToShopping.innerHTML = list[i].product;
-        addToShopping.innerHTML = '<li class="sub-list"> Category: ' + list[i].category + '</li>';
-        // addToShopping.innerHTML = '<button class="btn btn-outline-danger btn-sm mr-4" onclick="removeItem();">Remove</button>';
-      addToShopping.innerHTML = '</li>';
-    addToShopping.innerHTML = '</ul>';
+    addToShopping.innerHTML += '<ul>';
+      addToShopping.innerHTML += '<li>';
+      addToShopping.innerHTML += list[i].product;
+        addToShopping.innerHTML += '<li class="sub-list"> Category: ' + list[i].category + '</li>';
+      addToShopping.innerHTML += '</li>';
+    addToShopping.innerHTML += '</ul>';
+    addToShopping.innerHTML += '<button class="btn btn-outline-danger btn-sm" onclick="removeItem();">Remove</button>';
   }
 }
 
@@ -55,19 +55,21 @@ function addItem(x){
   list.push(list[x]);
   document.getElementById('itemInput').value = '';
 
+  // Tag turns into category for string to be displayed on screen
+
   var addToShopping = document.getElementById('shoppingList');
   addToShopping.innerHTML += '<li class="mt-2">';
   addToShopping.innerHTML += list[x];
-    // addToShopping.innerHTML += '<li class="sub-list"> Category: ' + list[x].tags + '</li>';
+    addToShopping.innerHTML += '<li class="sub-list"> Category: ' + category + '</li>';
   addToShopping.innerHTML += '</li>';
   addToShopping.innerHTML += '<button class="btn btn-outline-danger btn-sm" onclick="removeItem();">Remove</button>';
 };
 
 // function removeItem(){
-//   list.splice(0, list[x]);
+//   list.splice(0, list.product);
 // };
 
-// function filterItems(optionSelected){
+// function filterItems(category){
 //   const result = list.filter(x.value = list.category);
 //   console.log(result)
 // }
