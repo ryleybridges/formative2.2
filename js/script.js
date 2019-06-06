7b3329950fce61653bc31944edcf03128c70fe8d
Ryley anne Bridges <932400076@bun2-206-12.local>
2019-06-07 09:03:23 +1200

    Updated

--- js/script.js
+++ js/script.js
@@ -1,28 +1,53 @@
 var x = 0;
-var array = [];
+var list = [
+  {
+    product: "Apples",
+    category: "Produce"
+  },
+  {
+    product: "Chicken",
+    category: "Meat"
+  },
+  {
+    product: "Mince",
+    category: "Meat"
+  }
+];
 
-function addItem(optionSelected){
-  array[x] = document.getElementById('itemInput').value;
-  var optionSelected = document.getElementById('category').value;
+for (var i = 0; i < list.length; i++) {
+  // var addToShopping = document.getElementById('shoppingList');
+  // addToShopping.innerHTML = '<ul>';
+  //   addToShopping.innerHTML = '<li>';
+  //   addToShopping.innerHTML = list[i].product;
+      // addToShopping.innerHTML = '<li class="sub-list"> Category: ' + list[i].category + '</li>';
+  //   addToShopping.innerHTML = '</li>';
+  // addToShopping.innerHTML = '</ul>';
+  // document.getElementById('shoppingList').innerHTML = '<button class="btn btn-outline-danger btn-sm"><i class="fas fa-minus" onclick="removeItem();"></i></button>';
+}
+
+
+function addItem(x){
+  list[x] = document.getElementById('itemInput').value;
+  list.push(list[x]);
+  console.log(list);
+  // var optionSelected = document.getElementById('category').value;
   // array.push(array[x]);
   document.getElementById('itemInput').value = '';
 
   var addToShopping = document.getElementById('shoppingList');
-  addToShopping.innerHTML += '<ul>';
-  addToShopping.innerHTML += '<li>';
-  addToShopping.innerHTML += array[x];
-    addToShopping.innerHTML += '<li class="sub-list"> Category: ' + optionSelected + '</li>';
+  addToShopping.innerHTML += '<li class="mt-2">';
+  addToShopping.innerHTML += list[x];
+    // addToShopping.innerHTML += '<li class="sub-list"> Category: ' + optionSelected + '</li>';
   addToShopping.innerHTML += '</li>';
   addToShopping.innerHTML += '<button class="btn btn-outline-danger btn-sm"><i class="fas fa-minus" onclick="removeItem();"></i></button>';
-  addToShopping.innerHTML += '</ul>';
 
 };
 
-// function removeItem(){
-//   array.splice(0, array[x]);
-// };
+function removeItem(){
+  list.splice(0, list[x]);
+};
 
-function filterItems(x){
-  const result = array.filter(x.value = Meat);
-  console.log(result);
-}
+// function filterItems(optionSelected){
+//   const result = list.filter(x.value = list.category);
+//   console.log(result)
+// }
