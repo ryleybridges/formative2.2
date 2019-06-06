75086357113129138605f59384e0122fce30073a
Ryley anne Bridges <932400076@bun2-206-12.local>
2019-06-07 09:33:36 +1200

    Updated

--- js/script.js
+++ js/script.js
@@ -15,37 +15,57 @@ var list = [
 ];
 
 for (var i = 0; i < list.length; i++) {
-  // var addToShopping = document.getElementById('shoppingList');
-  // addToShopping.innerHTML = '<ul>';
-  //   addToShopping.innerHTML = '<li>';
-  //   addToShopping.innerHTML = list[i].product;
-      // addToShopping.innerHTML = '<li class="sub-list"> Category: ' + list[i].category + '</li>';
-  //   addToShopping.innerHTML = '</li>';
-  // addToShopping.innerHTML = '</ul>';
+  var addToShopping = document.getElementById('shoppingList');
+  addToShopping.innerHTML = '<ul>';
+    addToShopping.innerHTML = '<li>';
+    addToShopping.innerHTML = list[i].product;
+      addToShopping.innerHTML = '<li class="sub-list"> Category: ' + list[i].category + '</li>';
+    addToShopping.innerHTML = '</li>';
+  addToShopping.innerHTML = '</ul>';
   // document.getElementById('shoppingList').innerHTML = '<button class="btn btn-outline-danger btn-sm"><i class="fas fa-minus" onclick="removeItem();"></i></button>';
 }
+
+$(function(){
+  var availableTags = [
+    "Produce",
+    "Meat",
+    "Baked/Bread Products",
+    "Frozen Foods",
+    "Beverages",
+    "Canned/Jarred Goods",
+    "Dry/Baking Goods",
+    "Pet Items",
+    "Cleaning Products",
+    "Personal Care",
+    "Other"
+  ];
+
+  $("#tags").autocomplete({
+    source: availableTags
+  });
+});
 
 
 function addItem(x){
   list[x] = document.getElementById('itemInput').value;
   list.push(list[x]);
   console.log(list);
-  // var optionSelected = document.getElementById('category').value;
-  // array.push(array[x]);
+  var optionSelected = document.getElementById('category').value;
+  list.push(optionSelected);
   document.getElementById('itemInput').value = '';
 
   var addToShopping = document.getElementById('shoppingList');
   addToShopping.innerHTML += '<li class="mt-2">';
   addToShopping.innerHTML += list[x];
-    // addToShopping.innerHTML += '<li class="sub-list"> Category: ' + optionSelected + '</li>';
+    addToShopping.innerHTML += '<li class="sub-list"> Category: ' + optionSelected + '</li>';
   addToShopping.innerHTML += '</li>';
-  addToShopping.innerHTML += '<button class="btn btn-outline-danger btn-sm"><i class="fas fa-minus" onclick="removeItem();"></i></button>';
+  addToShopping.innerHTML += '<button class="btn btn-outline-danger btn-sm" onclick="removeItem();"><i class="fas fa-minus"></i></button>';
 
 };
 
-function removeItem(){
-  list.splice(0, list[x]);
-};
+// function removeItem(){
+//   list.splice(0, list[x]);
+// };
 
 // function filterItems(optionSelected){
 //   const result = list.filter(x.value = list.category);
